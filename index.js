b6de004eca14df563bb0f6c43a2f2f32409840b2
Stan <61481150+stanjdev@users.noreply.github.com>
2021-09-23 11:10:32 -0700

    simplified functions

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const inputs = document.querySelectorAll("input");
 const buttons = document.querySelectorAll('button');
 const nudgeInputs = document.querySelectorAll('.nudge-input');
 
+
+// Functions:
+function handleBill(e) {
+  let tipAmount = Number(bill.value * (tip.value / 100));
+  let totalPrice = Number(tipAmount) + Number(bill.value);
+  tipTotal.textContent = "$" + (tipAmount / numPeople.value).toFixed(2);
+  total.textContent = "$" + (totalPrice / numPeople.value).toFixed(2);
+};
+
+function nudgeValue(inputName, action) {
+  let val = parseInt(document.querySelector(`#${inputName}`).value);
+  val = action == "increment" ? val + 1 : val - 1;
+  document.querySelector(`#${inputName}`).value = val;
+}
+
+
 // When typing numbers into inputs:
 inputs.forEach(input => {
   input.addEventListener('input', e => {
@@ -34,27 +50,10 @@ inputs.forEach(input => {
 nudgeInputs.forEach(element => {
   element.onclick = function(e) {
     if (e.target && e.target.classList.contains('decrement')) {
-      decrement(e.target.dataset.name)
+      nudgeValue(e.target.dataset.name, 'decrement')
     } else if (e.target && e.target.classList.contains('increment')) {
-      increment(e.target.dataset.name)
+      nudgeValue(e.target.dataset.name, 'increment')
     }
     handleBill();
   }
 })
-
-function handleBill(e) {
-  let tipAmount = Number(bill.value * (tip.value / 100));
-  let totalPrice = Number(tipAmount) + Number(bill.value);
-  tipTotal.textContent = "$" + (tipAmount / numPeople.value).toFixed(2);
-  total.textContent = "$" + (totalPrice / numPeople.value).toFixed(2);
-};
-
-function increment(input) {
-  let val = parseInt(document.querySelector(`#${input}`).value) + 1;
-  document.querySelector(`#${input}`).value = val
-};
-
-function decrement(input) {
-  let val = parseInt(document.querySelector(`#${input}`).value) - 1;
-  document.querySelector(`#${input}`).value = val;
-};
