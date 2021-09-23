2493a4cd591b6f92427cbc4560137e06ac7b02fd
Stan <61481150+stanjdev@users.noreply.github.com>
2021-09-23 11:00:24 -0700

    first push

---
+++ index.js
@@ -0,0 +1,67 @@
+const bill = document.querySelector('#bill');
+const tip = document.querySelector('#tip');
+const numPeople = document.querySelector('#num-people');
+
+const tipTotal = document.querySelector('#tip-total');
+const total = document.querySelector('#total');
+
+const leftColumn = document.querySelector('.column-left');
+
+const inputs = document.querySelectorAll("input");
+const buttons = document.querySelectorAll('button');
+const nudgeInputs = document.querySelectorAll('.nudge-input');
+
+// When typing numbers into inputs:
+// inputs.forEach(input => {
+//   input.addEventListener('input', e => {
+//     // console.log(e);
+//     let tipAmount = Number(bill.value * (tip.value / 100));
+//     let totalPrice = Number(tipAmount + bill.value);
+//     tipTotal.textContent = "$" + (tipAmount / numPeople.value).toFixed(2);
+//     total.textContent = "$" + (totalPrice).toFixed(2);
+//   })
+// })
+
+// When pressing + or - buttons:
+nudgeInputs.forEach(element => {
+  element.onclick = function(e) {
+    if (e.target && e.target.classList.contains('decrement')) {
+      decrement(e.target.dataset.name)
+      // console.log(e.target.dataset.name)
+    } else if (e.target && e.target.classList.contains('increment')) {
+      increment(e.target.dataset.name)
+      // console.log(e.target.dataset.name)
+    }
+    handleBill();
+  }
+})
+
+
+bill.addEventListener('input', e => {
+  handleBill();
+});
+
+tip.addEventListener('input', e => {
+  handleBill();
+});
+
+numPeople.addEventListener('input', e => {
+  handleBill();
+});
+
+function handleBill(e) {
+  let tipAmount = Number(bill.value * (tip.value / 100));
+  let totalPrice = Number(tipAmount) + Number(bill.value);
+  tipTotal.textContent = "$" + (tipAmount / numPeople.value).toFixed(2);
+  total.textContent = "$" + (totalPrice / numPeople.value).toFixed(2);
+};
+
+function increment(input) {
+  let val = parseInt(document.querySelector(`#${input}`).value) + 1;
+  document.querySelector(`#${input}`).value = val
+};
+
+function decrement(input) {
+  let val = parseInt(document.querySelector(`#${input}`).value) - 1;
+  document.querySelector(`#${input}`).value = val;
+};
